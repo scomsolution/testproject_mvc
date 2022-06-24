ebe812fd1d65ce10d05859cbfdd94313fd468256
scomsolution <sksingh.india.91@gmail.com>
2022-06-24 09:41:01 +0530

    TEST SURAT DONE
    24-06-2022

---
+++ TEST_MVC_2/Controllers/ValuesController.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TEST_MVC_2.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        [HttpPost]
+        [Route("Login")]
+        public JObject loginService([FromBody] JObject loginJson)
+        {
+            JObject retJson = new JObject();
+            string username = loginJson["username"].ToString();
+            string password = loginJson["password"].ToString();
+            if (username == "admin" && password == "admin")
+            {
+                retJson.Add(new JProperty("authentication ", "successful"));
+            }
+            else
+            {
+                retJson.Add(new JProperty("authentication ", "unsuccessful"));
+            }
+            return retJson;
+        }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TEST_PROJECT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TEST_PROJECT.MvcApplication" Language="C#" %>
